19179f0d129be4f80f0aeaa35338c7aad2c0e61b
padrawalaayesha <apadrawala5@gmail.com>
2023-08-29 23:12:09 +0530

    Add move Forward method

--- Chandrayan.java
+++ Chandrayan.java
@@ -3,11 +3,17 @@ public class Chandrayan {
     public void moveBackward(){
         position--;
     }
+    public void moveForward(){
+        position++;
+    }
     public static void main(String[] args){
         Chandrayan sp = new Chandrayan();
-        String[] commands = {"b"};
+        String[] commands = {"b","f"};
         for (String command : commands) {
             if(command =="b"){
+                sp.moveBackward();
+            }
+            else if(command =="f"){
                 sp.moveBackward();
             }
             else{
